820e99394129e45ef18f86db91e7a1c4208087a8
Leo <ezioatiar@gmail.com>
2021-12-10 03:11:03 +0800

    Fix plot

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION          = '1.0.3' 
+VERSION          = '1.0.4' 
 DESCRIPTION      = '3D and 2D Truss structural analysis'
 
 
@@ -12,7 +12,7 @@ setup(
         author_email="ezioatiar@gmail.com",
         description=DESCRIPTION,
         url="https://github.com/leo27945875/Python_Stable_3D_Truss_Analysis",
-        download_url="https://github.com/leo27945875/Python_Stable_3D_Truss_Analysis/archive/refs/tags/v1.0.3.tar.gz",
+        download_url="https://github.com/leo27945875/Python_Stable_3D_Truss_Analysis/archive/refs/tags/v1.0.4.tar.gz",
         packages=['slientruss3d'],
         install_requires=['numpy', 'matplotlib'], 
         keywords=['python', 'truss', 'civil engineering', 'structural analysis'],
